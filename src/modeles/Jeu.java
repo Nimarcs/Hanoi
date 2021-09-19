b8f5a50ce9cb030a0859b86c8a76f6086da148d9
RICHIER Marcus <marcus.richier8@etu.univ-lorraine.fr>
2021-09-19 15:38:31 +0200

    Ajut de javadoc
    ajout de javadoc et de ommentaire pour resolution

--- src/modeles/Jeu.java
+++ src/modeles/Jeu.java
@@ -71,22 +71,36 @@ public class Jeu extends Observable {
         return tours.avoirGagner();
     }
 
+    /**
+     * methode permettant de resoudre automatiquement des tours du meme nombre de disque
+     * n'utilise pas l'avancement du joueur
+     */
     public void resolutionAuto(){
         demarrerPartie(tours.getNbDisque());
         resolutionRecursive(tours.getNbDisque(), Tours.tourGauche, Tours.tourDroite, Tours.tourMilieu);
     }
 
+    /**
+     * methode qui resout recursivement le probleme de 0
+     * @param n nombre de disque
+     * @param dep numero de la tour de depart
+     * @param arr numero de la tour d'arrive
+     * @param inter numero de la tour intermediaire
+     */
     public void resolutionRecursive(int n, int dep, int arr, int inter){
         if (n == 1) {
-            try {
+            try {//on essaie de bouger le disque
                 bougerDisque(dep, arr); //faire une liste ?
-            } catch (DisqueTropGrandException e){
+            } catch (DisqueTropGrandException e){ //n'est pas cense arrive
                 e.printStackTrace();
             }
         }else {
             resolutionRecursive(n-1, dep, inter, arr);
+            //on deplace tous les disque sauf le plus grand de la tour de depart a la tour intermediaire
             resolutionRecursive(1, dep, arr, inter);
+            //puis on deplace le plus grand disque a l'arrive
             resolutionRecursive(n-1, inter, arr, dep);
+            //puis on repose tous les autres disque par dessus
         }
     }
 
